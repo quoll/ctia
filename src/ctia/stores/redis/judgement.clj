514b0abb1dde284b9e9515311945fd105e205b60
Paula Gearon <pgearon@cisco.com>
2016-04-06 15:29:48 -0400

    Basic framework for storing judgements in redis. Not indexed

---
+++ src/ctia/stores/redis/judgement.clj
@@ -0,0 +1,36 @@
+(ns ctia.stores.ctia.judgement
+  (:require [ctia.schemas.common :as c]
+            [ctia.schemas.judgement
+             :refer [NewJudgement StoredJudgement realize-judgement]]
+            [ctia.schemas.relationships :as rel]
+            [ctia.schemas.verdict :refer [Verdict]]
+            [ctia.store :refer [IJudgementStore list-judgements]]
+            [taoensso.carmine :as c]
+            [schema.core :as s]))
+
+(def server1-conn {:pool {} :spec {:host "127.0.0.1" :port 6379}})
+(defmacro wcar [& body] `(c/wcar server1-conn ~@body))
+
+(defn create-judgement
+  [this login {:keys [id] :as new-judgement}]
+  (wcar (c/set id new-judgement)))
+
+(defn read-judgement
+  [this id]
+  (wcar (c/get id)))
+
+(defn delete-judgement [this id]
+  (wcar (c/del id)))
+
+(defn list-judgements [this filter-map]
+  )
+
+(defn calculate-verdict
+  [this observable]
+  )
+
+(defn list-judgements-by-observable [this observable]
+  )
+
+(defn add-indicator-to-judgement [this judgement-id indicator-relationship])
+  )
