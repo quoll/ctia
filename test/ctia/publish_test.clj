e0371de743540f334a124c5872ec60ee70ee2fb8
Stephen Sloan <steve@stevesloan.com>
2016-04-19 15:34:42 -0700

    Test fixtures for publish-test

--- test/ctia/publish_test.clj
+++ test/ctia/publish_test.clj
@@ -4,7 +4,11 @@
             [ctia.events :as e]
             [ctia.stores.redis.store :as redis]
             [ctia.properties :as prop]
+            [ctia.test-helpers.core :as test-helpers]
             [schema.test :as st]))
+
+(use-fixtures :once test-helpers/fixture-properties:redis-store)
+(use-fixtures :each test-helpers/fixture-ctia)
 
 (deftest ^:integration test-events
   (e/init!)
@@ -18,14 +22,11 @@
       (Thread/sleep 100)   ;; wait until the go loop is done
       (is (= [] (map (comp :data :model) @results)))))
   (testing "Checking that Redis can be enabled at runtime"
-    (let [original-props @prop/properties
-          props (swap! prop/properties update-in [:ctia :store :redis] #(assoc % :enabled true))
-          results (atom [])]
+    (let [results (atom [])]
       (pub/init!)
       (pub/event-subscribe #(swap! results conj %))
       (e/send-create-event "tester" {} "TestModelType" {:data 1})
       (e/send-event {:owner "tester" :http-params {} :model {:data 2}})
       (e/send-event {:owner "tester" :http-params {} :model {:data 3}})
       (Thread/sleep 100)   ;; wait until the go loop is done
-      (is (= [1 2 3] (map (comp :data :model) @results)))
-      (swap! prop/properties original-props))))
+      (is (= [1 2 3] (map (comp :data :model) @results))))))
