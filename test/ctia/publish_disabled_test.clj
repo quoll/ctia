edf51fc63e8cadef9152809359cd59f9f2a9e1d0
Stephen Sloan <steve@stevesloan.com>
2016-04-19 15:56:03 -0700

    Split up publish tests to use different fixtures

--- test/ctia/publish_disabled_test.clj
+++ test/ctia/publish_disabled_test.clj
@@ -1,21 +1,19 @@
-(ns ctia.publish-test
+(ns ctia.publish-disabled-test
   (:require [clojure.test :refer :all]
             [ctia.publish :as pub]
             [ctia.events :as e]
             [ctia.test-helpers.core :as test-helpers]))
 
-(use-fixtures :once test-helpers/fixture-properties:redis-store)
 (use-fixtures :each test-helpers/fixture-ctia)
 
-(deftest ^:integration test-events
+(deftest ^:integration test-events-with-redis-disabled
   (e/init!)
   (pub/init!)
-  (testing "Checking that Redis can be enabled at runtime"
+  (testing "check that an uninitialized redis does not attempt callbacks"
     (let [results (atom [])]
-      (pub/init!)
       (pub/event-subscribe #(swap! results conj %))
       (e/send-create-event "tester" {} "TestModelType" {:data 1})
       (e/send-event {:owner "tester" :http-params {} :model {:data 2}})
       (e/send-event {:owner "tester" :http-params {} :model {:data 3}})
       (Thread/sleep 100)   ;; wait until the go loop is done
-      (is (= [1 2 3] (map (comp :data :model) @results))))))
+      (is (= [] (map (comp :data :model) @results))))))
